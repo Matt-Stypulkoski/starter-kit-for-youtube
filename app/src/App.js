b0aa404077cbcfd9163aaa908721ebb859fa5143
Matt-Stypulkoski <matt.stypulkoski@gmail.com>
2021-01-09 15:58:40 -0500

    Display title, views, and thumbnail for video results

--- app/src/App.js
+++ app/src/App.js
@@ -3,9 +3,45 @@ import { React, Component } from 'react';
 import axios from 'axios';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.youtubeSearch = this.youtubeSearch.bind(this);
+    }
+
+    displayResults(results) {
+        let resultsContainer = document.getElementById("results-container");
+        let resultsContainerChildren = resultsContainer.children;
+        resultsContainerChildren[0].remove();
+        let resultsList = document.createElement("ul");
+
+
+        for (let vid of results) {
+            let videoListElement = document.createElement("li");
+            let videoDataContainer = document.createElement("div");
+            videoListElement.appendChild(videoDataContainer);
+
+            const views = document.createElement("p");
+            views.innerHTML = vid.statistics.viewCount;
+
+            const title = document.createElement("p");
+            title.innerHTML = vid.snippet.title;
+
+            const thumbnailURL = document.createElement("img");
+            thumbnailURL.src = vid.snippet.thumbnails.high.url;
+            thumbnailURL.alt = "Thumbnail for {vid.snippet.title}";
+
+            videoDataContainer.appendChild(title);
+            videoDataContainer.appendChild(views);
+            videoDataContainer.appendChild(thumbnailURL);
+
+            resultsList.appendChild(videoListElement);
+        }
+
+        resultsContainer.appendChild(resultsList);
+    }
 
     youtubeSearch() {
-        var keyword = document.getElementById("search-keyword").value;
+        let keyword = document.getElementById("search-keyword").value;
         if (keyword === "") {
             console.log("You must input a keyword to search for.");
             return
@@ -23,21 +59,22 @@ class App extends Component {
         })
             .then(res => {
                 console.log(res);
-                var videoList = res.data.items;
-                var vidIdList = [];
-                for (var i = 0; i < videoList.length; i++) {
-                    var vidId = videoList[i].id.videoId;
+                let videoList = res.data.items;
+                let vidIdList = [];
+                for (let i = 0; i < videoList.length; i++) {
+                    let vidId = videoList[i].id.videoId;
                     vidIdList.push(vidId);
                 }
                 return axios.get(`https://youtube.googleapis.com/youtube/v3/videos`, {
                     params: {
-                        "part": 'statistics',
+                        "part": "snippet,statistics",
                         "id": vidIdList.join(),
                         "key": process.env.REACT_APP_YOUTUBE_API_KEY,
                     }
                 })
-                    .then(result => {
-                        console.log(result);
+                    .then((result) => {
+                        console.log(result.data.items);
+                        this.displayResults(result.data.items); 
                     });
             })
     }
@@ -47,6 +84,9 @@ class App extends Component {
             <div className="App">            
                 <input type="text" id="search-keyword" placeholder="Input Keyword Here" required />
                 <button onClick={this.youtubeSearch}>Search</button>
+                <div className="results-container" id="results-container">
+                    <p className="placeholder-text">Search for a keyword to display results</p>
+                </div>
             </div>
         );
     }
