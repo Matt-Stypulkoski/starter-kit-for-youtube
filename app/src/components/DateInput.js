bb6d1638456c59cf324db5f051e88c4219950a96
Matt-Stypulkoski <matt.stypulkoski@gmail.com>
2021-02-11 17:30:09 -0500

    Change date selection to react-datepicker component

--- app/src/components/DateInput.js
+++ app/src/components/DateInput.js
@@ -1,36 +1,36 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
-function DateInput(props){
-
-    function getTodaysDate() {
-        let today = new Date();
-        let day = String(today.getDate()).padStart(2, '0');
-        let month = String(today.getMonth() + 1).padStart(2, '0');
-        let year = today.getFullYear();
-        return `${year}-${month}-${day}`
-    }
+function DateInput(props) {
+    const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
 
     return (
         <div className="date-container">
             <span>Date Range: </span>
-            <span>
-                <input type="date" id="published-after" />
-                <span> to </span>
-                <input type="date" id="published-before" />
-            </span>
+            <DatePicker
+                placeholderText="Select Start Date"
+                selected={startDate}
+                onChange={date => setStartDate(date)} />
+            <span> to </span>
+            <DatePicker
+                placeholderText="Select End Date"
+                selected={endDate}
+                onChange={date => setEndDate(date)} />
             <div className="date-tooltip">
                 <FontAwesomeIcon icon={faInfoCircle} />
                 <span class="tooltip-text">
                     The date parameter causes the search to
                     only return videos published between the
-                    two dates. One or both fields can be left blank. 
+                    two dates. One or both fields can be left blank.
                     If left blank, the search will not take that date
                     boundary into account.
                 </span>
             </div>
-        </div>
+    </div>
     )
 }
 
