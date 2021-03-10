9146c20e97c5f93a0b890ab8ee117297becf6ff6
Matt-Stypulkoski <matt.stypulkoski@gmail.com>
2021-03-09 20:34:33 -0500

    Rearrange order of elements in video-result-container to have thumbnail be first. Remove unnecessary text from video-result-container. Condense channel name and views into single element. Mirror Youtube's layout for mobile

--- app/src/components/VideoResult.js
+++ app/src/components/VideoResult.js
@@ -4,14 +4,14 @@ function VideoResult(props) {
 	return (
 		<li className="video-list-element">
 			<div className="video-result-container">
-				<div className="video-data-container">
-					<h3 className="video-title">{props.title}</h3>
-					<h3>By: <a href={'https://www.youtube.com/channel/' + props.channelId} target="_blank">{props.channelTitle}</a></h3>
-					<h4 className="video-views">Views: {parseInt(props.views).toLocaleString()}</h4>
-				</div>
 				<div>
 					<a id={props.videoId} className="video-link" target="_blank" onClick={() => setVideoUrl(props.videoId)}><img className="video-thumbnail" src={props.thumbnailURL} alt={props.altText} /></a>
-					<h5>Watch the video</h5>
+				</div>
+				<div className="video-data-container">
+					<h3 className="video-title">{props.title}</h3>
+					<div className="video-channel-views">
+						<h4><a href={'https://www.youtube.com/channel/' + props.channelId} target="_blank">{props.channelTitle}</a><span> - </span>{parseInt(props.views).toLocaleString()} views</h4>
+					</div>
 				</div>
 			</div>
 		</li>
